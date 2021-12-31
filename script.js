62b8bc5890b5bd2925d50b4f0ceefae61bf12107
evgenia-tkachenko <evgeniatkachenko24@gmail.com>
2021-12-31 12:22:11 +0300

    added becoming millionaire

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+var millionaire;
+
 function raiseSalary() {
-    console.log("raise!!")
     var salaryDiv = document.getElementById("mySalaryValue");
     var currentSalary = Number.parseInt(salaryDiv.textContent);
     var newSalary = currentSalary + Math.floor(Math.random() * 2000) + 50;
     salaryDiv.textContent = newSalary;
+
+    if (newSalary > 1000000 && !millionaire) {
+        becomeMillionaire();
+    }
+}
+
+function becomeMillionaire() {
+    alert("You are a millionaire!");
+    millionaire = true;
 }
 
 function setSalary() {
     var myDiv = document.getElementById("mySalaryValue");
     myDiv.textContent = Math.floor(Math.random() * 5000) + 200;
+    millionaire = false;
 }
+
 window.onload = setSalary;
