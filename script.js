8c6f933b6176d31de86325a570fea97eb5836c88
evgenia-tkachenko <evgeniatkachenko24@gmail.com>
2021-12-31 12:45:53 +0300

    added lose everything functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var millionaire;
+var bancrupt;
 
 function raiseSalary() {
     var salaryDiv = document.getElementById("mySalaryValue");
@@ -6,20 +7,40 @@ function raiseSalary() {
     var newSalary = currentSalary + Math.floor(Math.random() * 2000) + 50;
     salaryDiv.textContent = newSalary;
 
-    if (newSalary > 1000000 && !millionaire) {
+    if (newSalary > 10000 && !millionaire && !bancrupt) {
         becomeMillionaire();
+    }
+
+    if (newSalary > 20000 && millionaire && !bancrupt) {
+        loseEverything()
     }
 }
 
 function becomeMillionaire() {
-    alert("You are a millionaire!");
     millionaire = true;
+    var millionaireDiv = document.getElementById("myMillionaireStatus");
+    millionaireDiv.textContent = "You are a millionaire!!!"
+}
+
+function loseEverything() {
+    var millionaireDiv = document.getElementById("myMillionaireStatus");
+    millionaireDiv.textContent = "Oh shit! You lost everything 'cause you're one greedy bastard!";
+
+    var moneyBtn = document.getElementById("moneyBtn");
+    moneyBtn.remove();
+
+    var salaryLabel = document.getElementById("mySalaryLabel");
+    salaryLabel.remove()
+
+    var salaryDiv = document.getElementById("mySalaryValue");
+    salaryDiv.remove();
 }
 
 function setSalary() {
     var myDiv = document.getElementById("mySalaryValue");
     myDiv.textContent = Math.floor(Math.random() * 5000) + 200;
     millionaire = false;
+    bancrupt = false;
 }
 
 window.onload = setSalary;
